c0c39abadd07e19aaf3e82e4ee153dddc4e27705
César <dev.pcesarmata@gmail.com>
2020-08-19 13:09:01 -0500

    Init commit

---
+++ src/main/java/com/cesarmata/ws28restfulwebservicesvalidations/Ws28RestfulWebServicesValidationsApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws28restfulwebservicesvalidations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws28RestfulWebServicesValidationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws28RestfulWebServicesValidationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/ws28restfulwebservicesvalidations/Ws28RestfulWebServicesValidationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws28restfulwebservicesvalidations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws28RestfulWebServicesValidationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
